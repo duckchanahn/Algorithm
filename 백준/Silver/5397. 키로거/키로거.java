5b0489df1dd5b9bb89c4d7fb71f5bd74923aaa97
sanghyuk Kim <tkdgur5031@naver.com>
2025-01-13 19:49:03 +0900

    [Silver II] Title: 키로거, Time: 572 ms, Memory: 137388 KB -BaekjoonHub

--- 백준/Silver/5397. 키로거/키로거.java
+++ 백준/Silver/5397. 키로거/키로거.java
@@ -1,60 +1,100 @@
-// 시간복잡도: O(N) - N은 입력 문자열의 길이
+// 시간복잡도: O(L) - L은 입력 문자열의 길이
 
 import java.io.*;
-import java.util.Stack;
 
 public class Main {
-    public static String getPassword(String input) {
-        Stack<Character> left = new Stack<>();
-        Stack<Character> right = new Stack<>();
-        
-        for(char c : input.toCharArray()) {
-            switch(c) {
-                case '<':
-                    if(!left.empty()) {
-                        right.push(left.pop());
-                    }
-                    break;
-                case '>':
-                    if(!right.empty()) {
-                        left.push(right.pop());
-                    }
-                    break;
-                case '-':
-                    if(!left.empty()) {
-                        left.pop();
-                    }
-                    break;
-                default:
-                    left.push(c);
-                    break;
-            }
-        }
-        
-        while(!left.empty()) {
-            right.push(left.pop());
-        }
-        
-        StringBuilder sb = new StringBuilder();
-        while(!right.empty()) {
-            sb.append(right.pop());
-        }
-        
-        return sb.toString();
-    }
-    
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        
-        int T = Integer.parseInt(br.readLine());
-        
-        while(T-- > 0) {
-            String input = br.readLine();
-            bw.write(getPassword(input) + "\n");
-        }
-        
-        bw.flush();
-        bw.close();
-    }
+   static class Node {
+       char data;
+       Node prev, next;
+       
+       Node(char data) {
+           this.data = data;
+           this.prev = null;
+           this.next = null;
+       }
+   }
+   
+   static class LinkedList {
+       Node head;
+       Node tail;
+       Node cursor;
+       
+       LinkedList() {
+           head = new Node('H');
+           tail = new Node('T');
+           head.next = tail;
+           tail.prev = head;
+           cursor = tail;
+       }
+       
+       void insert(char data) {
+           Node newNode = new Node(data);
+           newNode.prev = cursor.prev;
+           newNode.next = cursor;
+           cursor.prev.next = newNode;
+           cursor.prev = newNode;
+       }
+       
+       void delete() {
+           if(cursor.prev != head) {
+               cursor.prev.prev.next = cursor;
+               cursor.prev = cursor.prev.prev;
+           }
+       }
+       
+       void moveLeft() {
+           if(cursor.prev != head) {
+               cursor = cursor.prev;
+           }
+       }
+       
+       void moveRight() {
+           if(cursor != tail) {
+               cursor = cursor.next;
+           }
+       }
+       
+       String getPassword() {
+           StringBuilder sb = new StringBuilder();
+           Node current = head.next;
+           while(current != tail) {
+               sb.append(current.data);
+               current = current.next;
+           }
+           return sb.toString();
+       }
+   }
+   
+   public static void main(String[] args) throws IOException {
+       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+       BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+       
+       int testCases = Integer.parseInt(br.readLine());
+       
+       while(testCases-- > 0) {
+           String input = br.readLine();
+           LinkedList list = new LinkedList();
+           
+           for(char c : input.toCharArray()) {
+               switch(c) {
+                   case '<':
+                       list.moveLeft();
+                       break;
+                   case '>':
+                       list.moveRight();
+                       break;
+                   case '-':
+                       list.delete();
+                       break;
+                   default:
+                       list.insert(c);
+               }
+           }
+           
+           bw.write(list.getPassword() + "\n");
+       }
+       
+       bw.flush();
+       bw.close();
+   }
 }
