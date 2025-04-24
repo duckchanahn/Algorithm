6a888c133979ce550585ebfe9fb6585757dafcc3
sanghyuk Kim <tkdgur5031@naver.com>
2025-04-24 09:52:23 +0900

    [Silver I] Title: 그림, Time: 396 ms, Memory: 48816 KB -BaekjoonHub

--- 백준/Silver/1926. 그림/그림.java
+++ 백준/Silver/1926. 그림/그림.java
@@ -2,47 +2,73 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    
+    private static int[][] map;
+    private static boolean[][] visited;
+    private static int[] dx = {1, -1, 0, 0};
+    private static int[] dy = {0, 0, 1, -1};
+    private static int n, m;
+    
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int[] size = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-
-        int[][] map = new int[size[0]][size[1]];
-        boolean[][] visit = new boolean[size[0]][size[1]];
-        for(int i = 0; i < size[0]; i++) {
-            map[i] = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-        }
-
-        Queue<int[]> q = new LinkedList<>();
-
-        int[] dx = { 1, -1, 0, 0};
-        int[] dy = { 0, 0, 1, -1};
-        int count = 0;
-        int max = 0;
-
-        for(int x = 0; x < size[0]; x++) {
-            for(int y = 0; y < size[1]; y++) {
-                if(visit[x][y] || map[x][y] == 0) continue;
-                q.offer(new int[] { x, y });
-                int extent = 1;
-                visit[x][y] = true;
-                while(!q.isEmpty()) {
-                    int[] now = q.poll();
-                        for(int i = 0; i < 4; i++) {
-                            int nextX = now[0] + dx[i];
-                            int nextY = now[1] + dy[i];
-                            if(nextX < 0 || size[0] <= nextX || nextY < 0 || size[1] <= nextY) continue;
-                            if(!visit[nextX][nextY] && map[nextX][nextY] == 1) {
-                                q.offer(new int[] {nextX, nextY});
-                                visit[nextX][nextY] = true;
-                                extent++;
-                            }
-                        }
-                }
-                count += 1;
-                max = Math.max(max, extent);
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        
+        map = new int[n][m];
+        visited = new boolean[n][m];
+        
+        for(int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for(int j = 0; j < m; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
             }
         }
-
-        System.out.print(count + "\n" + max);
+        
+        int answer = 0;
+        int max = 0;
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+                if(map[i][j] == 1 && !visited[i][j]) {
+                    max = Math.max(max, bfs(i, j));
+                    answer++;
+                }
+            }
+        }
+        
+        System.out.println(answer + "\n" + max);
+    }
+    
+    
+    private static int bfs(int i, int j) {
+        Queue<int[]> q = new ArrayDeque<>();
+        q.add(new int[] {i, j});
+        visited[i][j] = true;
+        
+        int size = 1;
+        while(!q.isEmpty()) {
+            int[] now = q.poll();
+            
+            for(int idx = 0; idx < 4; idx++) {
+                int nx = now[1] + dx[idx];
+                int ny = now[0] + dy[idx];
+                
+                
+                if(
+                    0 <= nx
+                    && nx < m
+                    && 0 <= ny
+                    && ny < n
+                    && !visited[ny][nx]
+                    && map[ny][nx] == 1
+                ) {
+                    q.add(new int[] {ny, nx});
+                    visited[ny][nx] = true;
+                    size++;
+                }
+            }
+        }
+        
+        return size;
     }
 }
