879a2893b842eb3d0f2aa49a42e45ed21066048a
sanghyuk Kim <tkdgur5031@naver.com>
2025-06-16 15:20:38 +0900

    [Gold IV] Title: 자두나무, Time: 104 ms, Memory: 14272 KB -BaekjoonHub

--- 백준/Gold/2240. 자두나무/자두나무.java
+++ 백준/Gold/2240. 자두나무/자두나무.java
@@ -2,50 +2,39 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    static int T, W;
-    static int[] fruits;
-    static int[][] dp;
-
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        // 입력 처리
-        st = new StringTokenizer(br.readLine());
-        T = Integer.parseInt(st.nextToken());
-        W = Integer.parseInt(st.nextToken());
+        int T = Integer.parseInt(st.nextToken());
+        int W = Integer.parseInt(st.nextToken());
 
-        fruits = new int[T + 1];  // 1초부터 T초까지
-        for (int i = 1; i <= T; i++) {
-            fruits[i] = Integer.parseInt(br.readLine());  // 1 또는 2
+        int[] map = new int[T+1];
+        for(int i = 1; i <= T; i++) {
+            map[i] = Integer.parseInt(br.readLine());
         }
 
-        // DP 테이블 초기화
-        dp = new int[T + 1][W + 1];
+        int[][] dp = new int[T+1][W+1];
 
-        // DP 수행
-        for (int t = 1; t <= T; t++) {
-            for (int w = 0; w <= W; w++) {
-                // 이전 상태 유지
-                dp[t][w] = dp[t - 1][w];
+        for(int i = 1; i <= T; i++) {
+            for(int j = 0; j <= W; j++) {
 
-                // 이동이 가능한 경우
-                if (w > 0) {
-                    dp[t][w] = Math.max(dp[t][w], dp[t - 1][w - 1]);
+                if(j > 0) {
+                    dp[i][j] = Math.max(dp[i-1][j], dp[i-1][j-1]);
+                } else {
+                    dp[i][j] = dp[i-1][j];
                 }
 
-                // 현재 위치가 자두 떨어지는 위치와 같으면 +1
-                int currTree = (w % 2 == 0) ? 1 : 2;
-                if (fruits[t] == currTree) {
-                    dp[t][w]++;
+                //0 2 4 = 1
+                if((j % 2 == 0 && map[i] == 1) || (j % 2 == 1 && map[i] == 2)) {
+                    dp[i][j] += 1;
                 }
             }
         }
 
-        // 정답: T초 동안 w번 이동했을 때의 최대값
         int answer = 0;
-        for (int w = 0; w <= W; w++) {
-            answer = Math.max(answer, dp[T][w]);
+        for(int i = 0; i <= W; i++) {
+            answer = Math.max(answer, dp[T][i]);
         }
 
         System.out.println(answer);
